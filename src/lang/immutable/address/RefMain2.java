bc36704a6faa875ee7a4cb04a204679bd1311854
ccyhyeok <calvinchun@naver.com>
2024-07-27 13:54:18 +0900

    불변객체 20240727

---
+++ src/lang/immutable/address/RefMain2.java
@@ -0,0 +1,20 @@
+package lang.immutable.address;
+
+public class RefMain2 {
+
+    public static void main(String[] args) {
+        //참조형 변수는 하나의 인스턴스를 공유할 수 있다.
+        ImmutableAddress a = new ImmutableAddress("서울");
+        // 참조값 대입을 막을 수 있는 방법은 없음(Java 문법상 아무문제 없이 맞음)
+        // a가  x001 참조 시 b도 x001도 참고한다.
+        ImmutableAddress b = a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        //b.setValue("부산"); // b의 값을 부산으로 변경해야함.
+        b = new ImmutableAddress("부산");
+        System.out.println("부산 -> b");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
